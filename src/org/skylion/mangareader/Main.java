6fc7652fc75359096bb1db74e266b159d8f99d7e
Skylion007 <skylion.aaron@gmail.com>
2013-11-26 16:43:52 -0500

    Updated L&F to Nimbus for crossplatform stability of custom JComponents.

--- src/org/skylion/mangareader/Main.java
+++ src/org/skylion/mangareader/Main.java
@@ -2,6 +2,7 @@ package org.skylion.mangareader;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 import org.skylion.mangareader.gui.MainGUI;
 
@@ -9,7 +10,14 @@ import org.skylion.mangareader.gui.MainGUI;
 public class Main {
 	public static void main(String[] args) {
 		try{
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			//Loads the Nimbus look and feel
+			//Retrieval method is convulted, but stable.
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
 		}catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -18,5 +26,6 @@ public class Main {
          gui.pack();//Sizes the Frame
          gui.setExtendedState(gui.getExtendedState()|JFrame.MAXIMIZED_BOTH );//Makes it full screen
 	}
+
 }
 
